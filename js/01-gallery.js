3ce3b8618a6715d124c4c5e1e1dfcd23176f4ab3
VikaSemenchuk <viktoriia.semenchuk@gmail.com>
2023-01-23 01:40:14 +0100

    task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+
+function createGalleryMarkup(items) {
+  return items
+    .map((item) =>
+        `<div class="gallery__item"> 
+           <a class"gallery__item-link" href="${item.original}">
+             <img class="gallery__item-image" src="${item.preview}"
+               data-sourse="${item.original}"
+               alt="${item.description}"/>
+           </a>
+        </div>`
+    )
+    .join("");
+}
+const addGalleryMarkup = createGalleryMarkup(galleryItems);
+gallery.innerHTML = addGalleryMarkup;
+gallery.addEventListener("click", onImageClick);
+
+function onImageClick(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.sourse}" width="800" height="600">
+`);
+
+  instance.show();
+
+  gallery.addEventListener("keydown", (event) => {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  });
+}
 
 console.log(galleryItems);
